afec8bc2f878dd4ecc9755f6c634de37c620fa77
Funmi <emily.yahoo.co.uk@gmail.com>
2024-10-25 01:13:20 +0100

    Adding build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+	build: {
+	  rollupOptions: {
+		// https://rollupjs.org/configuration-options/
+	  },
+	},
+  })
